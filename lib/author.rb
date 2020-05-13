3080131001c0624c0419ca9f97d9770b4a16c74c
Joseph Romero <joeromero47@gmail.com>
2020-05-13 22:00:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -16,7 +16,7 @@ class Author
   end
 
   def posts 
-      post.all.select {|post| post.author == self}
+      Post.all.select {|post| post.author == self}
    end
   def add_post_by_title(title)
     post = post.new(title)
