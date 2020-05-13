fbb81acce32ac1016b2f3c9dae9d40f7d872c4f0
Joseph Romero <joeromero47@gmail.com>
2020-05-13 21:59:54 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,33 @@
+class Author
+
+  attr_accessor :name, :all
+
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    Post.author = self
+    @@post_count += 1
+  end
+
+  def posts 
+      Post.all.select {|post| post.author == self}
+   end
+  def add_post_by_title(title)
+    post = post.new(title)
+    @posts << post
+    Post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    post 
+    post.all
+    post.all.count
+  end
+end
